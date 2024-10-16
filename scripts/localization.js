3d85da05ec79581e2b9f4041ea1c736739a902dc
MakeThingsSimple <jonahtablet21@gmail.com>
2024-10-16 20:17:20 +0800

    Added component system, cleaned up various files

--- scripts/localization.js
+++ scripts/localization.js
@@ -1,32 +1,29 @@
-async function localize(language) {
-    const language_switcher = document.querySelector("#language-selector")
-    const avaliable_localizations = ["en-gb", "zh-tw"]
-    let localization_language = "en-gb"
+async function localize(preference = null) {
+    let avaliableLocalizations = ["en-gb", "zh-tw"]
+    let localizationLanguage = "en-gb"
+    let languageSwitcher = document.querySelector("#language-selector")
 
-    avaliable_localizations.forEach((value, index, array) => {
-        if (language.toLowerCase() === value) {
-            localization_language = language.toLowerCase()
-        }
-    })
+    if (preference == null) {
+        let netLanguage = navigator.language
 
-    language_switcher.value = localization_language
+        avaliableLocalizations.forEach((value, index, array) => {
+            if (netLanguage.toLowerCase() === value) {
+                localizationLanguage = netLanguage.toLowerCase()
+            }
+        })
+    } else if (avaliableLocalizations.findIndex((element) => element == preference) != -1) {
+        localizationLanguage = preference
+    }
 
-    let res = await fetch(`../localization/${localization_language}.json`)
+    console.log(`Localizing document in ${localizationLanguage}`)
+
+    languageSwitcher.value = localizationLanguage
+
+    let res = await fetch(`../localization/${localizationLanguage}.json`)
     let obj = await res.json()
-    strings = Object.values(obj)[0]
+    let strings = Object.values(obj)[0]
 
-    let elements = document.querySelectorAll('[data-text]')
-
-    elements.forEach((value, key) => {
+    document.querySelectorAll('[data-text]').forEach((value, key) => {
         value.innerHTML = strings[value.dataset.text]
     })
-}
-
-async function localize_element(element_query, language) {
-    let res = await fetch(`../localization/${language.toLowerCase()}.json`)
-    let obj = await res.json()
-    strings = Object.values(obj)[0]
-
-    let element = document.querySelector(element_query)
-    element.setHTML(strings[element.dataset.text])
 }
